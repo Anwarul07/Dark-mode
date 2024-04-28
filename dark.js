9d19b1b787dbf1073e9e270d8f40d69c7e2c343e
Anwarul07 <mdanwarul064@gmail.com>
2024-04-29 00:07:54 +0530

    Update index.html

--- dark.js
+++ dark.js
@@ -1,1 +1,12 @@
 console.log("feel the dark theme");
+
+let icon=document.getElementById("icon");
+icon.addEventListener(  "click", ()=>{
+    document.body.classList.toggle("dark-theme")
+    if (document.body.classList.contains("dark-theme")) {
+        icon.src="files/sun.png";
+        
+    } else{
+        icon.src="files/moon.png";
+    }
+})
